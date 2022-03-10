0343a8d20cd483f6850b96e6b110b30bdf87accc
JustinPugh01 <97039184+JustinPugh01@users.noreply.github.com>
2022-03-10 00:46:16 +0000

    more sql added

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,7 @@ public class Main {
             insertrestaurants.executeUpdate();
         }
     }
-        public static void mains()throws Exception{
-        new DB("jdbc:sqlite:./db.sql/sql.db");
-        PreparedStatement insertrestaurants = DB.conn.prepareStatement("INSERT INTO restaurants (name, imageURL) VALUES (?,?);");
-        insertrestaurants.setString(1, "FiveGuys");
-        insertrestaurants.setString(2, "fiveguys.co.uk");
-        insertrestaurants.executeUpdate();
-    }
+
+    
     
 }
--- src/Main2.java
+++ src/Main2.java
@@ -5,10 +5,23 @@ public class Main2 {
     public static void main(String[] args) throws Exception {
 
         new DB("jdbc:sqlite:./db.sql/sql.db");
+        PreparedStatement insertmenu = DB.conn.prepareStatement("INSERT INTO MENUS (restaurant_id, name) VALUES (?, ?);");
+        insertmenu.setInt(1, 6);
+        insertmenu.setString(2, "DinnerBuffet");
+        insertmenu.executeUpdate();
+
+        /*new DB("jdbc:sqlite:./db.sql/sql.db");
+        PreparedStatement insertdishes = DB.conn.prepareStatement("INSERT INTO dishes (menu_id, name) VALUES (?,?);");
+        insertdishes.setInt(1, 1);
+        insertdishes.setString(2, "Half Chicken and 2 sides");
+        insertdishes.executeUpdate();*/
+
+
+        /*new DB("jdbc:sqlite:./db.sql/sql.db");
         PreparedStatement insertrestaurants = DB.conn.prepareStatement("INSERT INTO restaurants (name, imageURL) VALUES (?,?);");
         insertrestaurants.setString(1, "FiveGuys");
         insertrestaurants.setString(2, "fiveguys.co.uk");
-        insertrestaurants.executeUpdate();
+        insertrestaurants.executeUpdate();*/
     }
 
 }
